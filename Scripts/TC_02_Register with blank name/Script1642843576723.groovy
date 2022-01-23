a45db0c06e496e21ab709ef8d7d5c898521b7fb0
quynh <quynh@PNVL073.pn.com.vn>
2022-01-23 12:51:11 +0700

    hocMaiPage

---
+++ Scripts/TC_02_Register with blank name/Script1642843576723.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(linkWeb)
+
+WebUI.click(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_nametxt'))
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_emailtxt'), email)
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_phonetxt'), phone)
+
+WebUI.setEncryptedText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_passwordtxt'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setEncryptedText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_repaswordtxt'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.click(findTestObject('Page_ng k nhp hc - HOCMAI/TC_02_signUpButton'))
+
+WebUI.closeBrowser()
+
---
+++ Scripts/TC_07_Register without password/Script1642846639424.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(linkWeb)
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_name'), name)
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_emailtxt'), email)
+
+WebUI.setText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_phonetxt'), phone)
+
+WebUI.click(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_passwordtxt'))
+
+WebUI.setEncryptedText(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_repaswordtxt'), 'D6aKfyiHY1zPzifvV+TXPA==')
+
+WebUI.click(findTestObject('Page_ng k nhp hc - HOCMAI/TC_07_signUpButton'))
+
+WebUI.closeBrowser()
+
